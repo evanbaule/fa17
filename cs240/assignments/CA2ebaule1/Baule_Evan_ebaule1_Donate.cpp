929ed0518283202dc655b1c81b92bdcb53a9ae6b
Evan M. Baule <evanbaule@gmail.com>
2017-09-19 17:31:11 -0400

    this is gonna take some more thought

--- cs240/assignments/CA2ebaule1/Baule_Evan_ebaule1_Donate.cpp
+++ cs240/assignments/CA2ebaule1/Baule_Evan_ebaule1_Donate.cpp
@@ -10,11 +10,13 @@ int main(int argc, char *argv[])
 
 	//Establish max length of load file from command line
 	string filename;
-	int maxLen = 0;
+	int maxLen = 1;
 	if(argc > 0){
 		fileName = argv[0];
 		maxLen = argv[1];
 	}
+
+	DonorDatabase * db = new DonorDatabase(maxLen);
 
 	/* Prompt */
 	string inputCommand;
@@ -27,19 +29,36 @@ int main(int argc, char *argv[])
 			string userIdInput;
 			cout << "Enter your userID: " << endl;
 			cin >> userIdInput;
-			if(donorDB->searchUID(userIdInput)){
-				if(
+			if(db->searchUID(userIdInput)){
+				if(db->login(userIdInput)){
+					string internalInputCommand;
+					while(internalInputCommand != "Logout"){
+						cout << "Enter a command: " << endl;
+						cin >> internalInputCommand;
+
+						if(internalInputCommand == "Manage")
+						{
+										
+						}
+						if(internalInputCommand == ""){
+
+						}
+						if(internalInputCommand == ""){
+
+						}
+						if(internalInputCommand == ""){
+
+						}
+						if(internalInputCommand == ""){
+
+						}
+						if(internalInputCommand == ""){
+
+						}
+					}
+				}	
 			}
-
-
-				string internalInputCommand;
-				while(internalInputCommand != "Logout")
-				cout << "Enter a command: " << endl;
-				cin >> internalInputCommand;
-		}
-
-		
-
+		}	
 	}
 
 	//deconstruct references
